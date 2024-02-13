65552d682e8e94b6f7b8ad61522bfb4e68358eb5
John W <114405644+JohnRWinsor@users.noreply.github.com>
2024-02-12 21:52:51 -0400

    initial commit

---
+++ A02/MP3Player/MP3Player/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MP3Player
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private MediaPlayer mediaPlayer;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            mediaPlayer = new MediaPlayer();
+        }
+
+        private void Play_Click(object sender, RoutedEventArgs e)
+        {
+            mediaPlayer.Play();
+        }
+
+        private void Pause_Click(object sender, RoutedEventArgs e)
+        {
+            mediaPlayer.Pause();
+        }
+
+        private void Stop_Click(object sender, RoutedEventArgs e)
+        {
+            mediaPlayer.Stop();
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void SelectMp3File_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+            dlg.DefaultExt = ".mp3";
+            dlg.Filter = "MP3 Files (*.mp3)|*.mp3";
+
+            bool? result = dlg.ShowDialog();
+
+            if (result == true)
+            {
+                mediaPlayer.Open(new Uri(dlg.FileName));
+            }
+        }   
+    }
+}
